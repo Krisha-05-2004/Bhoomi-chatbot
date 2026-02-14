7c729433a6dfee364cb18fed083f990e29bc374b
Krisha-05-2004 <saikrisha05@gmail.com>
2026-02-14 11:01:54 +0530

    Move requirements to root

--- BACKEND/src/llm.py
+++ BACKEND/src/llm.py
@@ -1,10 +1,7 @@
 from langchain_groq import ChatGroq
-import os
+
 def get_llm():
     return ChatGroq(
         model="llama-3.1-8b-instant",
         temperature=0.3
-          groq_api_key=os.getenv("GROQ_API_KEY")
-    )     
-        
-    
+    )
