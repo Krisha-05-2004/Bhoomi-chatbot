05a7b0db5928d3c0a00c7694667a2a4b52172a66
Krisha-05-2004 <saikrisha05@gmail.com>
2026-02-14 12:46:49 +0530

    DONE

--- BACKEND/src/rag_pipeline.py
+++ BACKEND/src/rag_pipeline.py
@@ -14,7 +14,7 @@ from config import RAW_DATA_DIR, VECTORSTORE_DIR
 
 def build_rag_pipeline(rebuild=True):
     embeddings = HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
+        model_name="intfloat/multilingual-e5-base"
     )
 
     index_file = VECTORSTORE_DIR / "index.faiss"
@@ -32,7 +32,10 @@ def build_rag_pipeline(rebuild=True):
         vectorstore = FAISS.from_documents(chunks, embeddings)
         vectorstore.save_local(str(VECTORSTORE_DIR))
 
-    retriever = vectorstore.as_retriever(search_kwargs={"k": 4})
+    retriever = vectorstore.as_retriever(
+        search_type="mmr",
+        search_kwargs={"k": 4, "fetch_k": 10}
+        )
     template = """
     You are Bhoomi 🌱, a smart and friendly farming expert.
 
