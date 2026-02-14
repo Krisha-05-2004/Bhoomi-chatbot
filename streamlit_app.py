25d77900377db9c088ea65085a50433ad0ac62f8
Krisha-05-2004 <saikrisha05@gmail.com>
2026-02-14 11:25:25 +0530

    Move requirements to root

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,18 @@
 import streamlit as st
 from BACKEND.src.rag_pipeline import build_rag_pipeline
+import re
 
+def load_css_from_html():
+    with open("FRONTEND/templates/index.html", "r", encoding="utf-8") as f:
+        html = f.read()
+
+    # Extract CSS inside <style>...</style>
+    css = re.search(r"<style>(.*?)</style>", html, re.DOTALL)
+    
+    if css:
+        st.markdown(f"<style>{css.group(1)}</style>", unsafe_allow_html=True)
+
+load_css_from_html()
 st.title("🌾 Bhoomi - Smart Farming Assistant")
 
 @st.cache_resource
